4ac6e270637d839c17bb7bc2da8fdf36822f1ad9
Umutcan <umutcanguller_26@hotmail.com>
2022-11-02 03:11:16 +0300

    Create AnaSayfa.php

---
+++ AnaSayfa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<HTML>
+    <Head>
+
+        <title>AdminPaneli</title>
+
+    </Head>
+
+    <body>
+        
+        <?php
+
+            echo 'Giriş Yapıldı';
+            echo "<br />";
+            echo "<br />";
+            echo "<a href='admin.php'>Çıkış Yap</a>";
+
+
+        ?>
+        
+
+    </body>
+
+
+
+
+</HTML>
