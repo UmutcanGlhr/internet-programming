4c24fa01dde84d662f14ebcd413911562d683962
Umutcan <umutcanguller_26@hotmail.com>
2022-11-02 03:11:08 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<HTML>
+    <Head>
+
+        <title>AdminPaneli</title>
+
+    </Head>
+
+    <body>
+        
+        <form action="sonuc.php" method="post">
+            KullanıcıAdı : <br /><input type="text" name="AdminAdi"><br /><br />
+            Şifre : <br /><input type="password" name="AdminSifre"><br/><br/>
+            <input type="submit" value="Giriş">
+
+
+        </form>
+
+
+    </body>
+
+
+
+
+</HTML>
