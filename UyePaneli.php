1eb2ec30f4308584ad9867fec3fd9eee38e7689a
Umutcan <umutcanguller_26@hotmail.com>
2022-11-02 03:10:54 +0300

    Create UyePaneli.php

---
+++ UyePaneli.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<HTML>
+    <Head>
+
+        <title>AdminPaneli</title>
+
+    </Head>
+
+    <body>
+        
+        <form action="sonuc1.php" method="post">
+            KullanıcıAdı : <br /><input type="text" name="KullaniciAdi"><br /><br />
+            Şifre : <br /><input type="password" name="KullaniciSifre"><br/><br/>
+            <input type="submit" value="Giriş">
+
+
+        </form>
+
+
+    </body>
+
+
+
+
+</HTML>
